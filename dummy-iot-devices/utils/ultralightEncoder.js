83aa4a6553a6b366d95ab19221dcc69dd9ef4f15
yudhnaaaaa <hoanganhduy75@gmaill.com>
2025-12-05 03:23:06 +0700

    feat: update *.js header file

--- dummy-iot-devices/utils/ultralightEncoder.js
+++ dummy-iot-devices/utils/ultralightEncoder.js
@@ -1,3 +1,11 @@
+/*
+ * Open-Terra - IoT and Smart City Data Platform
+ * @author Vibe Coders / HCMCOU
+ * @copyright (C) 2025 Vibe Coders / HCMCOU. All rights reserved
+ * @license MIT License
+ * @see https://github.com/dinhduongdev/Open-Terra The Open-Terra GitHub project
+ */
+
 /**
  * UltraLight 2.0 Protocol Encoder
  * Converts key-value pairs to UltraLight format
